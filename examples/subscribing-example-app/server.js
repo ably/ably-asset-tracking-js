7a0d1fc12b4b3c5ddb8e43686c91afb39af2a4a8
Owen Pearson <owen.pearson@ably.com>
2020-12-04 14:14:43 +0000

    add server from example app

---
+++ examples/subscribing-example-app/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const Ably = require('ably/promises');
+require('dotenv').config();
+
+if (!process.env.ABLY_API_KEY) {
+  console.error('ABLY_API_KEY environment variable is missing');
+  process.exit(1);
+}
+
+const client = new Ably.Realtime(process.env.ABLY_API_KEY);
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.get('/', async (request, response) => {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/mapbox', async (request, response) => {
+  response.sendFile(__dirname + '/views/mapbox.html');
+});
+
+app.get('/google', async (request, response) => {
+  response.sendFile(__dirname + '/views/google.html');
+});
+
+app.get('/api/createTokenRequest', async (request, response) => {
+  const tokenRequestData = await client.auth.createTokenRequest({
+    clientId: 'ably-client-side-api-calls-demo',
+  });
+  response.send(tokenRequestData);
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
