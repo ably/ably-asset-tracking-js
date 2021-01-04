4ba06b659e3a163f7dcb7464ffaa2080f3cb7c0c
Owen Pearson <owen.pearson@ably.com>
2021-01-04 14:41:28 +0000

    prompt users to enter their own map provider keys

--- examples/subscribing-example-app/public/Google/script.js
+++ examples/subscribing-example-app/public/Google/script.js
@@ -3,8 +3,12 @@ import { RiderConnection } from "../RiderConnection.js";
 import { Coordinate } from "../Coordinate.js";
 import { bindUi } from "../Ui.js";
 
-(async function() {
+var script = document.createElement('script');
+const apiKey = prompt('Please enter your Google Maps API key');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=&v=weekly&callback=initMap`;
+script.defer = true;
 
+async function createMap() {
   const position = new Coordinate(0, 0);
   const mapElement = document.getElementById("map");
   const map = new google.maps.Map(mapElement, { center: position, zoom: 3 });
@@ -17,4 +21,8 @@ import { bindUi } from "../Ui.js";
   await riderConnection.connect(); 
   
   bindUi(riderConnection);   
-})();
+};
+
+window.initMap = createMap;
+
+document.head.appendChild(script);
--- examples/subscribing-example-app/public/Mapbox/script.js
+++ examples/subscribing-example-app/public/Mapbox/script.js
@@ -3,14 +3,17 @@ import { RiderConnection } from "../RiderConnection.js";
 import { Coordinate } from "../Coordinate.js";
 import { bindUi } from "../Ui.js";
 
-(async function() {  
+const accessToken = prompt('Please enter your Mapbox access token');
+mapboxgl.accessToken = accessToken;
+
+(async function createMap() {
   const position = new Coordinate(0, 0);
   const mapElement = "map";
   const map = new mapboxgl.Map({ center: position.toGeoJson(), zoom: 15, container: mapElement, style: 'mapbox://styles/mapbox/streets-v11' });
 
   function createMarker(coordinate) {
     return new MapBoxMarker(map, coordinate);
-  }
+  };
 
   const riderConnection = new RiderConnection(createMarker);
   await riderConnection.connect();
