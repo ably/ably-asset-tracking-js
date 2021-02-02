b8ddd5a239e41040282d356867ea551de2830ca2
Owen Pearson <owen.pearson@ably.com>
2021-02-02 08:44:55 +0000

    add support files for integration tests

---
+++ test/integration/index.ts
@@ -0,0 +1,10 @@
+import CustomEventReporter from './support/CustomEventReporter';
+import mochaHooks from './support/mochaHooks';
+
+mocha.timeout(10_000);
+
+// This will search for files ending in .test.js and require them
+// so that they are added to the webpack bundle
+const ctx = require.context('.', true, /.+\.test\.ts?$/);
+ctx.keys().forEach(ctx);
+module.exports = ctx;
